40c422a1f6fda16a8bf21b3d99cc78dfc5c6d939
daniel <carlos.daniel2016@hotmail.com>
2020-09-28 22:22:08 -0300

    Primeiras alteracoes

--- js/javascript.js
+++ js/javascript.js
@@ -2,54 +2,51 @@
 $.fn.extend({
     quiz: function(json) {
       console.log(json)
+        var dados = json;
         var index = 0;
         var correct = 0;
-        var indexes = '';
         var items = '';
-        for (var i = 0; i <= 12; i++) {
-            if (i == 0) {
-                indexes += `<li data-target="#carousel" class="active"></li>`;
-                items += 
-                `
-                <div class="carousel-item active">
-                    <div class="item">
-                      <dic class="col">
-                        <div class="row d-flex justify-content-center">
-                          <img id="logo" src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" width="200px">
-                        </div>
-                        <h2 id="info">E se as chances de contrair o novo coronavírus forem proporcionais ao seu conhecimento sobre ele?</h2>
-                        <h3 id="subinfo">Responda a 20 afirmações sobre a COVID-19 e descubra neste game quão seguro você está!</h3>
-                        <div class="row d-flex justify-content-center mt-4">
-                          <button class="button btn-warning avancar" id="start-button">Iniciar</button>
-                        </div>
-                        <div id="responsaveis">
-                          Responsáveis técnicos: </br>
-                          Dr. Claudio L. S. Ferrari - CRM: 60113 - SP </br>
-                          Dra. Márcia M. da Costa - CRM: 76624 - SP
-                        </div>
-                      </dic>
-                    </div>
+        indexes = `<li data-target="#carousel" class="active not-visible"></li>`;
+          items += 
+          `
+          <div class="carousel-item active">
+              <div class="item">
+                <div class="col">
+                  <div class="row d-flex justify-content-center">
+                    <img id="logo" src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" width="200px">
+                  </div>
+                  <h2 id="info">E se as chances de contrair o novo coronavírus forem proporcionais ao seu conhecimento sobre ele?</h2>
+                  <h3 id="subinfo">Responda a 20 afirmações sobre a COVID-19 e descubra neste game quão seguro você está!</h3>
+                  <div class="row d-flex justify-content-center mt-4">
+                    <button class="button btn-warning avancar" id="start-button">Iniciar</button>
+                  </div>
+                  <div id="responsaveis">
+                    Responsáveis técnicos: </br>
+                    Dr. Claudio L. S. Ferrari - CRM: 60113 - SP </br>
+                    Dra. Márcia M. da Costa - CRM: 76624 - SP
+                  </div>
                 </div>
-                `;
-            } else {
+              </div>
+          </div>
+          `;
+        for (var i in json) {                
                 indexes += `<li data-target="#carousel"></li>`;
                 items += 
                 `
                 <div class="carousel-item">
                     <div class="item">
-                      <dic class="col">
-                        <h2>${ json[0]['title']}</h2>
+                      <div class="col">
+                        <h2 class="title">${ dados[i]['title']}</h2>
                         <div class="row d-flex justify-content-center mt-4 options">
                           <button class="button true mr-4 select" value="0">Verdadeiro</button>
                           <button class="button false select" value="1">Falso</button>
                         </div>
-                      </dic>
+                      </div>
                     </div>
                 </div>
                 `;
-            }
-            
         }
+
         var pattern = 
         `
         <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="false">
@@ -62,29 +59,22 @@ $.fn.extend({
           </div>
         </div>
         <!-- End Caroussel -->
-        <button class="btn btn-primary" id="avancar">Avançar</button>
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal">
-          Launch
-        </button>
         
         <!-- Modal -->
         <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title"></h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-              </div>
+              
               <div class="modal-body">
-                
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <section id="content"></section>
+                <div class="row d-flex justify-content-center">
+                  <button type="button" class="btn btn-primary" id="next">Próxima</button>
+                </div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" id="next">Save</button>
-              </div>
+              
             </div>
           </div>
         `;
@@ -117,35 +107,50 @@ $.fn.extend({
           
           
           //$(document).quiz();
+          
+            console.log(json.length)
           $('.select').on('click', function() {
+            if (index < json.length) {
             modal = ''
-            if ($(this).val() == json[0]['correct']['feedback']) {
-              console.log(1);
+            if ($(this).val() == json[index-1]['correct']['feedback']) {
               correct++
-              modal += 
+              modal = 
               `
-              
+              <div class="row d-flex justify-content-center">
+                <i class="far fa-check-circle icons" id="correct"></i>
+              </div>
+              ${ json[index-1]['correct']['note'] }
               `;
             } else {
-              console.log(2);
-              correct++
-              index++
-              modal += 
+              modal = 
               `
-              
+              <div class="row d-flex justify-content-center">
+                <i class="far fa-times-circle icons" id="false"></i>
+              </div>
+              ${ json[index-1]['correct']['note'] }
               `;
             }
+            index++
             // Modal
-            $('.modal-title').html('Teste');
-            $('.modal-body').html(modal);
+            $('#content').html( modal);
             $('#modal').modal('show');
 
             // Avançar
-            $('#next').click(function() {
-              $('#carousel').carousel('next');
-              $('#modal').modal('hide');
-            });
+            
+              console.log(index)
+              $('#next').click(function() {
+                $('#carousel').carousel('next');
+                $('#modal').modal('hide');
+              });
+              $('.close').click(function() {
+                $('#carousel').carousel('next');
+                $('#modal').modal('hide');
+              });
+            }
           });
+        
+
+          
           // $('#avancar').on('click',function() {;
           //     if (index < 20) {
       
